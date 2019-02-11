b42d4534bb6a78a2ae0c7fa573a01e8ba3f87249
Natesuri <natesuri@gmail.com>
2019-02-11 09:53:51 -0500

    Reverts api endpoint from plot_plants to plots
    Changes the getPlots api endpoint from plot_plants to plots to reflect
    and change to the api's structure. This commit could be squashed into
    the one before this?

--- assets/scripts/plot/api.js
+++ assets/scripts/plot/api.js
@@ -4,7 +4,7 @@ const userStore = require('../userStore.js')
 const getPlots = function () {
   const userToken = userStore.user.user.token
   return $.ajax({
-    url: config.apiUrl + '/plot_plants',
+    url: config.apiUrl + '/plots',
     method: 'GET',
     headers: {Authorization: `Token token=${userToken}`}
   })
